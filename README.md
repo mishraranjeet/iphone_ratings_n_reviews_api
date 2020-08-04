1a2cfadd1dec9d179ce0a2624a505ef730c7c103
uxcam <uxcam@uxcams-MacBook-Pro.local>
2020-08-04 23:45:06 +0545

    impromvents

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # iphone_ratings_n_reviews_api
-This is API written in django-restframework that returns data include different Iphones and their ratings and reviews by scrapping data from website.
+This is API written in django-restframework that returns data include different Iphones and their ratings and reviews by scrapping data from website flipkart.
 
 ## Technology Used
 Backend : python/Django
 Library: Beautifulsoup4
+Database: Postgresql
--- iphone_rating_api/settings.py
+++ iphone_rating_api/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'iphone_rating_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'iphoneapi_database',
-#         'USER': 'iphoneapi',
-#         'PASSWORD':'iphoneapi',
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3'
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'iphoneapi_database',
+        'USER': 'iphoneapi',
+        'PASSWORD':'iphoneapi',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3'
+
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
